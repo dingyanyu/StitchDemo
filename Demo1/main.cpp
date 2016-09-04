0b38261b9b75cd7050a0e2d1814874748b8ec324
lincaiming <lincaiming5837@qq.com>
2016-09-04 21:46:05 +0800

    second commit migrate the program

--- Demo1/main.cpp
+++ Demo1/main.cpp
@@ -16,8 +16,22 @@ int parseCmdArgs(int argc, char** argv);
 
 int main(int argc, char* argv[])
 {
-	int retval = parseCmdArgs(argc, argv);
-	if (retval) return -1;
+//	int retval = parseCmdArgs(argc, argv);
+//	if (retval) return -1;
+
+	Mat img1 = imread("boat1.jpg");
+	Mat img2 = imread("boat2.jpg");
+	Mat img3 = imread("boat3.jpg");
+	Mat img4 = imread("boat4.jpg");
+	Mat img5 = imread("boat5.jpg");
+	Mat img6 = imread("boat6.jpg");
+
+	imgs.push_back(img1);
+	imgs.push_back(img2);
+	imgs.push_back(img3);
+	imgs.push_back(img4);
+	imgs.push_back(img5);
+	imgs.push_back(img6);
 
 	Mat pano;
 	Stitcher stitcher = Stitcher::createDefault(try_use_gpu);
@@ -28,68 +42,11 @@ int main(int argc, char* argv[])
 		cout << "Can't stitch images, error code = " << int(status) << endl;
 		return -1;
 	}
-
+	
+	namedWindow(result_name);
+	imshow(result_name, pano);
 	imwrite(result_name, pano);
+	waitKey();
 	return 0;
 }
 
-
-void printUsage()
-{
-	cout <<
-		"Rotation model images stitcher.\n\n"
-		"stitching img1 img2 [...imgN]\n\n"
-		"Flags:\n"
-		"  --try_use_gpu (yes|no)\n"
-		"      Try to use GPU. The default value is 'no'. All default values\n"
-		"      are for CPU mode.\n"
-		"  --output <result_img>\n"
-		"      The default is 'result.jpg'.\n";
-}
-
-
-int parseCmdArgs(int argc, char** argv)
-{
-	if (argc == 1)
-	{
-		printUsage();
-		return -1;
-	}
-	for (int i = 1; i < argc; ++i)
-	{
-		if (string(argv[i]) == "--help" || string(argv[i]) == "/?")
-		{
-			printUsage();
-			return -1;
-		}
-		else if (string(argv[i]) == "--try_use_gpu")
-		{
-			if (string(argv[i + 1]) == "no")
-				try_use_gpu = false;
-			else if (string(argv[i + 1]) == "yes")
-				try_use_gpu = true;
-			else
-			{
-				cout << "Bad --try_use_gpu flag value\n";
-				return -1;
-			}
-			i++;
-		}
-		else if (string(argv[i]) == "--output")
-		{
-			result_name = argv[i + 1];
-			i++;
-		}
-		else
-		{
-			Mat img = imread(argv[i]);
-			if (img.empty())
-			{
-				cout << "Can't read image '" << argv[i] << "'\n";
-				return -1;
-			}
-			imgs.push_back(img);
-		}
-	}
-	return 0;
-}
